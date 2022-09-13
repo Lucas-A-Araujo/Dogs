fe1fda4c5fcaadb38170f2b7023a8e9693e86545
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2022-09-13 16:14:59 -0300

    Updates

---
+++ precache-manifest.904c57d4cc2fe5631bb417576e63051a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1151b87df364ed43a7e398be900d11",
+    "url": "/Dogs/index.html"
+  },
+  {
+    "revision": "79b02abb5948976a923f",
+    "url": "/Dogs/static/css/4.bee358b8.chunk.css"
+  },
+  {
+    "revision": "babda0259c35fcdabae2",
+    "url": "/Dogs/static/css/main.06694b5a.chunk.css"
+  },
+  {
+    "revision": "e6048958b947d69952ea",
+    "url": "/Dogs/static/js/2.8c2d6067.chunk.js"
+  },
+  {
+    "revision": "13f75d441afac7d8e033",
+    "url": "/Dogs/static/js/3.aac3d68b.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/Dogs/static/js/3.aac3d68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79b02abb5948976a923f",
+    "url": "/Dogs/static/js/4.5571146b.chunk.js"
+  },
+  {
+    "revision": "babda0259c35fcdabae2",
+    "url": "/Dogs/static/js/main.8b25f0a1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Dogs/static/js/main.8b25f0a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8199a63c4d2e50247058",
+    "url": "/Dogs/static/js/runtime-main.4d2014d8.js"
+  },
+  {
+    "revision": "4a6d6ceb26b2d64c10a2b26fa9c47aaf",
+    "url": "/Dogs/static/media/adicionar.4a6d6ceb.svg"
+  },
+  {
+    "revision": "8cc31290254329d8d3990af08d454cd7",
+    "url": "/Dogs/static/media/dogs-footer.8cc31290.svg"
+  },
+  {
+    "revision": "849a64433f3bdd08d38623156960aa56",
+    "url": "/Dogs/static/media/dogs.849a6443.svg"
+  },
+  {
+    "revision": "9f80ad3de4a46815a44aa1a78bfc37dd",
+    "url": "/Dogs/static/media/enviar.9f80ad3d.svg"
+  },
+  {
+    "revision": "5b48807f51a9c4c87f8a5f802cf35aa0",
+    "url": "/Dogs/static/media/estatisticas.5b48807f.svg"
+  },
+  {
+    "revision": "35f798d01b74896299f27c50c3cd6816",
+    "url": "/Dogs/static/media/feed.35f798d0.svg"
+  },
+  {
+    "revision": "824d7d85ae3af9a43a61d46fe174192e",
+    "url": "/Dogs/static/media/login.824d7d85.jpg"
+  },
+  {
+    "revision": "4175ea7207f56280b48714a4133289cb",
+    "url": "/Dogs/static/media/sair.4175ea72.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/Dogs/static/media/usuario.dc442992.svg"
+  },
+  {
+    "revision": "957befc3d57033c55a88d144ae173685",
+    "url": "/Dogs/static/media/visualizacao-black.957befc3.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/Dogs/static/media/visualizacao.4ce15ed8.svg"
+  }
+]);
